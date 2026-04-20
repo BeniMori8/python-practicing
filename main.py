9378f217912273dd48fca239c97faa8f49e536f1
Ben Mor <benmor7711@gmail.com>
2026-04-20 11:54:56 +0300

    finished the task of 2023.1!

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+from advent_of_code_2023.aoc_23_1.aoc_23_1 import aoc_23_1_task
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    print("23.1 completed!")

550f9aa1e0fe84c924d2bf2dc5d16a5a73e187ce
Ben Mor <benmor7711@gmail.com>
2026-04-23 16:39:32 +0300

    finished task 4 of 2023

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from advent_of_code_2023.aoc_23_3.solution import aoc_23_3_task2
+from advent_of_code_2023.aoc_23_4.solution import aoc_23_4_task_part2
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print("finished aoc 2023.3 !")
+    print("finished aoc 2023 4!")
